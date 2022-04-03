689684fe1e6a2f2053997e520b9373b3533f9ff8
shapun <shapunspoonja963@gmail.com>
2022-04-03 13:58:27 +0530

    fix: remove warning by evaluating elvis operator first

--- app/src/main/java/com/shapun/apkaabconverter/adapter/MetaDataAdapter.kt
+++ app/src/main/java/com/shapun/apkaabconverter/adapter/MetaDataAdapter.kt
@@ -6,19 +6,21 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.shapun.apkaabconverter.databinding.RowMetaDataBinding
 import com.shapun.apkaabconverter.model.MetaData
+import kotlin.io.path.absolutePathString
 
 class MetaDataAdapter(data: List<MetaData>) : RecyclerView.Adapter<MetaDataAdapter.ViewHolder>() {
     private val mData: List<MetaData> = data
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        return ViewHolder(RowMetaDataBinding.inflate(LayoutInflater.from(parent.context),parent,false))
+        return ViewHolder(RowMetaDataBinding.inflate(LayoutInflater.from(parent.context), parent,false))
     }
 
     @SuppressLint("SetTextI18n")
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.binding.tvMetaFilePath.text = "File Name: "+mData[position].originalFileName?:mData[position].path.toString()
-        holder.binding.tvMetaDirectory.text= "Folder: "+mData[position].directory
-        holder.binding.tvMetaFileName.text = "FileName: "+mData[position].fileName
+        val data = mData[position]
+        holder.binding.tvMetaFilePath.text = "File: " + (data.originalFileName ?: data.path.absolutePathString())
+        holder.binding.tvMetaDirectory.text = "Folder: " + data.directory
+        holder.binding.tvMetaFileName.text = "File Name in AAB: " + data.fileName
     }
 
     override fun getItemCount(): Int = mData.size
