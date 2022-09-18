08751d5c143391795711f0741ed0a3506e871754
qingyu <qyma.yyy@qq.com>
2022-09-18 14:27:07 +0800

    Upgrade targetSDK to 33

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 }
 
 android {
-    compileSdk = 32
+    compileSdk = 33
     namespace = "com.shapun.apkaabconverter"
 
     defaultConfig {
         minSdk = 26
-        targetSdk = 32
+        targetSdk = 33
         versionCode = 2
         versionName = "1.1"
         applicationId = android.namespace
@@ -47,12 +47,12 @@ dependencies {
     implementation("com.android.tools.build:apksig:7.1.3")
     implementation("com.google.guava:guava:31.1-jre")
     implementation("com.google.protobuf:protobuf-java:3.21.2")
-    implementation("androidx.appcompat:appcompat:1.4.2")
+    implementation("androidx.appcompat:appcompat:1.5.1")
     implementation("com.google.android.material:material:1.6.1")
-    implementation("androidx.core:core-ktx:1.8.0")
+    implementation("androidx.core:core-ktx:1.9.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.3")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.4.1")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.4.1")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.5.1")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.5.1")
     implementation("androidx.legacy:legacy-support-v4:1.0.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.3")
