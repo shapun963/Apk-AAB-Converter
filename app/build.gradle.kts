267ddfbd0a6b364633b1c34c97378bde4894666b
yangyiyu08 <qyma.yyy@qq.com>
2023-03-23 01:48:52 +0800

    update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,8 +1,8 @@
 @file:Suppress("UnstableApiUsage")
 
 plugins {
+    kotlin("android")
     id("com.android.application")
-    id("org.jetbrains.kotlin.android")
 }
 
 android {
@@ -13,7 +13,7 @@ android {
         minSdk = 26
         targetSdk = 33
         versionCode = 2
-        versionName = "1.1"
+        versionName = "1.2"
         applicationId = android.namespace
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
@@ -43,20 +43,20 @@ android {
 }
 
 dependencies {
-    implementation(files("libs/jksprovider.jar")) //jks
-    implementation("org.bouncycastle:bcprov-jdk18on:1.71.1") //pkcs12
-    implementation("com.android.tools.build:bundletool:1.9.1")
-    implementation("com.android.tools.build:apksig:7.1.3")
+    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.8.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
+
     implementation("com.google.guava:guava:31.1-jre")
     implementation("com.google.protobuf:protobuf-java:3.21.2")
-    implementation("androidx.appcompat:appcompat:1.5.1")
-    implementation("com.google.android.material:material:1.6.1")
-    implementation("androidx.core:core-ktx:1.9.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.3")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.5.1")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.5.1")
-    implementation("androidx.legacy:legacy-support-v4:1.0.0")
+    implementation("com.android.tools.build:apksig:7.4.2")
+    implementation("com.android.tools.build:bundletool:1.14.0")
+    implementation("org.bouncycastle:bcprov-jdk15on:1.70")
+
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.3")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
